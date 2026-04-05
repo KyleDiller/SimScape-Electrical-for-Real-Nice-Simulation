d7c26321e6601361da8c50bf92825699f2b32e7c
Kyle Diller <254148075+KyleDiller@users.noreply.github.com>
2026-04-05 13:47:17 -0400

    Add files via upload

---
+++ SimulationCases/Case1_DClink_Current_PMSM_Drive/PMSMDynamicSwitches_init.m
@@ -0,0 +1,26 @@
+% Control parameters
+
+fsw = 2e3;   % Switching frequency (Hz)
+fc = fsw*10; % Control loop frequency (Hz)
+Tsc = 1/fc;  % Control loop sample time
+
+% PMSM parameters 
+PM = 0.03; % Permanent magnet flux linkage
+Ld = 2e-4;  % d-axis inductance
+Lq = 2e-4;  % q-axis inductance
+L0 = 2e-4;  % 0-axis inductance
+Rs = 0.013; % Stator resistance
+N = 6; % Number of pole pairs
+J = 0.2; % Moment of inertia
+
+% Operating and derived limits
+Imax = 100;  % Max stator current (peak value)
+Tmax = 1.5*N*PM*Imax; % Max lectromagnetic torque
+fnom = 100;  % Nominal frequency (Hz)
+rpm_nom = 60*fnom/N;      % Nominal rotor speed (rpm)
+omegam_nom = 2*pi*fnom/N; % Nominal mechanical rotor speed
+Pmax = omegam_nom*Tmax;     % Maximum power
+
+Gs = 4.4526;
+s = 2.8994;
+h = 2e-6;
